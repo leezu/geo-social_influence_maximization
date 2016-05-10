19612988239537327294bf3fc228eedb788e6334
Leonard Lausen <leonard@lausen.nl>
2016-05-10 15:37:48 +0800

    Add main.cpp and CMakeLists.txt
    main.cpp contains some basic code for docopt.
    CMakeLists.txt states the dependency on docopt and on the boost graph
    library.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "docopt.h"
+
+static const char USAGE[] =
+R"(Geo-social influence maximization
+
+    Usage:
+      gsinfmax (-h | --help)
+      gsinfmax --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+)";
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
