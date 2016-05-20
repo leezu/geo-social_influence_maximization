ff09aab3012dde761b04c1cde309c2151b5cc847
Leonard Lausen <leonard@lausen.nl>
2016-05-20 20:41:40 +0800

    Refactor file parsing code
    - Restructure reader code into classes
    - Use memory mapped files to speed up reading
    - Clean up X3 parsing to increase speed
    - Use bitset to keep track of nodes for which location was already added

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/spirit/home/x3.hpp>
-#include <boost/spirit/include/support_istream_iterator.hpp>
-#include <boost/fusion/sequence.hpp>
-#include <boost/fusion/include/sequence.hpp>
+#include <boost/fusion/adapted/std_tuple.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/dynamic_bitset.hpp>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <random>
 
 #include "docopt.h"
+
+namespace x3 = boost::spirit::x3;
+namespace fusion = boost::fusion;
 
 static const char USAGE[] =
 R"(Geo-social influence maximization
@@ -38,133 +39,95 @@ struct edge_property {
 // Define our graph
 // We use setS to enforce our graph not to become a multigraph
 typedef boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS, vertex_property, edge_property > graph;
-//Some typedefs for simplicity
-typedef boost::graph_traits<graph>::vertex_descriptor vertex_descr_t;
-typedef boost::graph_traits<graph>::edge_descriptor edge_descr_t;
+using vertex_descriptor = graph::vertex_descriptor;
+using edge_descriptor = graph::edge_descriptor;
 
-// Parse gowalla dataset
-void parse_gowalla(auto g, std::string edge_path, std::string location_path) {
-	namespace x3 = boost::spirit::x3;
-	namespace fusion = boost::fusion;
+class Reader {
+	protected:
+		graph g;
+};
 
-	std::unordered_map<int, vertex_descr_t>  vertices; // Store vertex descriptors
-	std::unordered_set<int>  location_already_added; // Store vertices, for which we already added location
+class Gowalla_reader : public Reader {
+	public:
+		bool read_edges(std::string fname) {
+			// Lambda function that adds edge to graph
+			auto add_edge = [&](auto& ctx){
+				vertex_descriptor source, target;
+				auto tup = std::tie(source, target);
 
-	// Random number generator for the edge weights
-	std::default_random_engine generator;
-	std::uniform_real_distribution<double> distribution(0.0,1.0);
+				// Add edge from the vertices returned from context
+				fusion::copy(x3::_attr(ctx), tup);
+				auto aer = boost::add_edge(source, target, g);
 
-	// Lambda function that adds vertex to graph if not already added
-	auto add_vertex = [&](auto& ctx){
-		// Return if the vertex is already known
-		if (vertices.find(x3::_attr(ctx)) != vertices.end())	{
-			return false;
+				edge_property edge {};
+				edge.weight = distribution(generator);
+
+				g[aer.first] = edge;
+			};
+
+			// Parse the gowalla edge file
+			boost::iostreams::mapped_file_source mm(fname);
+
+			auto f = mm.begin(), l = mm.end();
+
+			x3::parse(f, l, *((x3::int_ >> '\t' >> x3::int_ >> x3::eol)[add_edge]));
+
+			std::cout << "Parsed " << boost::num_vertices(g) << " vertices" << std::endl;
+			std::cout << "Parsed " << boost::num_edges(g) << " edges" << std::endl;
+
+			// Fail if we couldn't parse the whole edges file
+			return f == l;
 		}
 
-		// Otherwise add vertex to graph
-		auto v = boost::add_vertex(g);
+		bool read_locations(std::string fname) {
+			boost::dynamic_bitset<> location_already_added(num_vertices(g));
 
-		// And add vertex descriptor to map
-		vertices[x3::_attr(ctx)] = v;
-	};
+			auto add_location = [&](auto& ctx){
+				// _attr(ctx) returns a boost fusion tuple
+				auto attr = x3::_attr(ctx);
+				auto vertex_id = fusion::at_c<0>(attr);
 
-	// Lambda function that adds edge to graph
-	auto add_edge = [&](auto& ctx){
-		// _attr(ctx) returns a boost fusion tuple
-		auto attr = x3::_attr(ctx);
+				if (location_already_added.test(vertex_id)) {
+					return true; // Exit, as we already stored the location for this vertex
+				}
+				location_already_added.set(vertex_id);
 
-		// Add edge from the vertices returned from context
-		auto aer = boost::add_edge(vertices[fusion::at_c<0>(attr)],
-				vertices[fusion::at_c<1>(attr)], g);
+				// Test if vertex is in our graph
+				// We are parsing locations from a different file than the graph,
+				// so there might be inconsistencies
+				if (graph::null_vertex() == vertex_id) {
+					std::cerr << "Tried to add location to vertex " << vertex_id << ", but this vertex is not in our graph" << std::endl;
+					return false;
+				}
 
-		edge_property edge {};
-		edge.weight = distribution(generator);
+				// Add location to the vertex
+				auto& props = g[vertex_id];
+				props.latitude = fusion::at_c<1>(attr);
+				props.longitude = fusion::at_c<2>(attr);
 
-		g[aer.first] = edge;
-	};
+				return true;
+			};
 
-	// Lambda function that adds locations to vertices in the graph
-	auto add_location = [&](auto& ctx){
-		// _attr(ctx) returns a boost fusion tuple
-		auto attr = x3::_attr(ctx);
-		auto vertex_id = fusion::at_c<0>(attr);
+			boost::iostreams::mapped_file_source mm(fname);
 
-		if (location_already_added.find(vertex_id) != location_already_added.end())	{
-			// Exit, as we already stored the location for this vertex
-			return true;
+			auto f = mm.begin(), l = mm.end();
+
+			x3::parse(f, l,
+					// [vertex_id]          [time of checkin]              [latitude]             [longitude]                          [location] id
+					*((x3::int_ >> '\t' >> x3::omit[*x3::graph] >> '\t' >> x3::double_ >> '\t' >> x3::double_)[add_location] >> '\t' >> x3::int_ >> x3::eol)
+				 );
+
+			std::cout << "Added location to " << location_already_added.count() << " vertices" << std::endl;
+
+			// Fail if we couldn't parse the whole location file
+			return f == l;
 		}
-		location_already_added.insert(vertex_id);
 
-		// Test if vertex is in our graph
-		// We are parsing locations from a different file than the graph,
-		// so there might be inconsistencies
-		if (vertices.find(vertex_id) == vertices.end())	{
-			std::cerr << "Tried to add location to vertex " << vertex_id << ", but this vertex is not in our graph" << std::endl;
-			return false;
-		}
-
-		auto vertex = vertices[vertex_id];
-
-		// Add location to the vertex
-		g[vertex].latitude = fusion::at_c<2>(attr);
-		g[vertex].longitude = fusion::at_c<3>(attr);
-
-		return true;
-	};
-
-	std::ifstream edge_file(edge_path);
-	std::ifstream location_file(location_path);
-	if (!edge_file) {
-		std::cerr << "Couldn't open " << edge_path << " for reading";
-	}
-	if (!location_file) {
-		std::cerr << "Couldn't open " << location_path << " for reading";
-	}
-	// By default whitespace is skipped, so we disable that
-	edge_file >> std::noskipws;
-	location_file >> std::noskipws;
-
-	// Parse the gowalla edge file
-	boost::spirit::istream_iterator file_iterator(edge_file), eof;
-
-	x3::phrase_parse(file_iterator, eof,
-			// Begin grammar
-			(
-			 *((x3::int_[add_vertex] >> x3::int_[add_vertex])[add_edge])
-			),
-			// End grammar
-			x3::space
-			);
-
-	// Fail if we couldn't parse the whole edges file
-	if (file_iterator != eof) {
-		std::cerr << "Couldn't parse whole edges file" << std::endl;
-	}
-
-	// Parse the gowalla location file
-	file_iterator = boost::spirit::istream_iterator(location_file);
-
-	x3::phrase_parse(file_iterator, eof,
-			// Begin grammar
-			(
-			 // vertex_id 	time of checkin 	  latitude 	longitude 		      location id
-			 *((x3::int_ >> x3::lexeme[*x3::graph] >> x3::double_ >> x3::double_)[add_location] >> x3::int_ >> x3::eol)
-			),
-			// End grammar
-			x3::blank
-			);
-
-	// Fail if we couldn't parse the whole location file
-	if (file_iterator != eof) {
-		std::cerr << "Couldn't parse whole location file" << std::endl;
-	}
-
-	std::cout << "Parsed " << boost::num_vertices(g) << " vertices" << std::endl;
-	std::cout << "Parsed " << boost::num_edges(g) << " edges" << std::endl;
-	std::cout << "Added location to " << location_already_added.size() << " vertices" << std::endl;
-
-	return;
-}
+	private:
+		// Random number generator for the edge weights
+		std::default_random_engine generator;
+		std::uniform_real_distribution<double> distribution = std::uniform_real_distribution<double>(0.0, 1.0);
+};
 
 int main(int argc, char* argv[]) {
 	std::map<std::string, docopt::value> args
@@ -173,11 +136,13 @@ int main(int argc, char* argv[]) {
 				true,						// show help if requested
 				"geo-social influence maximzation 0.1");	// version string
 
-	graph g;
 
 	// Gowalla dataset
 	if (args.find("gowalla") != args.end()) {
-		parse_gowalla(g, args["<edges>"].asString(), args["<locations>"].asString());
+		auto reader = Gowalla_reader();
+
+		reader.read_edges(args["<edges>"].asString());
+		reader.read_locations(args["<locations>"].asString());
 	}
 
 	return 0;
