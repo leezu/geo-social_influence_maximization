f13f06096b44414cb21ea96a00145f5c3a8c6108
Leonard Lausen <leonard@lausen.nl>
2016-06-09 17:31:02 +0800

    analysis: add functions to compute node degrees of network

--- analysis.cpp
+++ analysis.cpp
@@ -1,6 +1,8 @@
 #include "analysis.hpp"
 
-#include "boost/filesystem.hpp"
+#include <boost/filesystem.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/iteration_macros.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -18,5 +20,19 @@ std::ofstream get_logfile(std::string fname, std::string directory) {
     }
 
     return std::ofstream(directory + "/" + fname);
+}
+
+double get_average_node_degree(const network &g) {
+    double node_degree{0};
+
+    BGL_FORALL_VERTICES(user, g, network) { node_degree += degree(user, g); }
+
+    return node_degree / num_vertices(g);
+}
+double write_node_degrees(const network &g, const std::string fname) {
+    auto f = get_logfile(fname);
+    BGL_FORALL_VERTICES(user, g, network) {
+        f << user << "\t" << degree(user, g) << "\n";
+    }
 }
 }
--- analysis.hpp
+++ analysis.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "Graph_reader.hpp"
+
 #include <fstream>
 #include <string>
 
 namespace gsinfmax {
 std::ofstream get_logfile(std::string fname, std::string directory = "./log");
+
+double get_average_node_degree(const network &g);
+double write_node_degrees(const network &g,
+                          const std::string fname = "node_degrees");
 }
